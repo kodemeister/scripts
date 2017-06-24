09876b2ad2037d4840ec05ba6f372769f5449715
toiffel <toiffel@inbox.ru>
2017-06-24 23:33:47 +0600

    Disable IPv6 via kernel command line

--- efiboot.sh
+++ efiboot.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-efibootmgr -c -d /dev/sda -p 1 -L "Arch Linux" -l /EFI/arch/vmlinuz-linux -u "root=/dev/mapper/vg_root-lv_root rw initrd=/EFI/arch/intel-ucode.img initrd=/EFI/arch/initramfs-linux.img intel_iommu=on nvidia-drm.modeset=1"
-efibootmgr -c -d /dev/sda -p 1 -L "Arch Linux (LVM Snapshots)" -l /EFI/arch/vmlinuz-linux -u "root=/dev/mapper/vg_root-lv_root rw initrd=/EFI/arch/intel-ucode.img initrd=/EFI/arch/initramfs-linux.img intel_iommu=on nvidia-drm.modeset=1 systemd.unit=lvm-snapshots.target"
+KERNEL_PARAMETERS="root=/dev/mapper/vg_root-lv_root rw initrd=/EFI/arch/intel-ucode.img initrd=/EFI/arch/initramfs-linux.img intel_iommu=on nvidia-drm.modeset=1 ipv6.disable=1"
+
+efibootmgr -c -d /dev/sda -p 1 -L "Arch Linux" -l /EFI/arch/vmlinuz-linux -u "$KERNEL_PARAMETERS"
+efibootmgr -c -d /dev/sda -p 1 -L "Arch Linux (LVM Snapshots)" -l /EFI/arch/vmlinuz-linux -u "$KERNEL_PARAMETERS systemd.unit=lvm-snapshots.target"
