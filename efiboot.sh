d1705a1dd5452c289fcf18cc433feacc491353a1
kodemeister <kodemeister@outlook.com>
2018-08-17 02:04:47 +0600

    Move boot files to the root of ESP partition

--- efiboot.sh
+++ efiboot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -r KERNEL_PARAMETERS="root=/dev/mapper/vg_root-lv_root rw initrd=/EFI/arch/intel-ucode.img initrd=/EFI/arch/initramfs-linux.img intel_iommu=on ipv6.disable=1"
+declare -r KERNEL_PARAMETERS="root=/dev/mapper/vg_root-lv_root rw initrd=/intel-ucode.img initrd=/initramfs-linux.img intel_iommu=on ipv6.disable=1"
 
-efibootmgr -c -d /dev/sda -p 1 -L "Arch Linux" -l /EFI/arch/vmlinuz-linux -u "$KERNEL_PARAMETERS"
-efibootmgr -c -d /dev/sda -p 1 -L "Arch Linux (LVM Snapshots)" -l /EFI/arch/vmlinuz-linux -u "$KERNEL_PARAMETERS systemd.unit=lvm-snapshots.target"
+efibootmgr -c -d /dev/sda -p 1 -L "Arch Linux" -l /vmlinuz-linux -u "$KERNEL_PARAMETERS"
+efibootmgr -c -d /dev/sda -p 1 -L "Arch Linux (LVM Snapshots)" -l /vmlinuz-linux -u "$KERNEL_PARAMETERS systemd.unit=lvm-snapshots.target"
