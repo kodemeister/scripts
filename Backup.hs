b9ba17613ee20aeac21e520da1394c0541119184
kodemeister <kodemeister@outlook.com>
2020-07-26 02:38:49 +0600

    Format source files with Ormolu

--- Backup.hs
+++ Backup.hs
@@ -4,108 +4,114 @@ import Turtle
 
 checkIfRoot :: IO ()
 checkIfRoot = do
-    userID <- getEffectiveUserID
-    when (userID /= 0) $ die "Please run me as root"
+  userID <- getEffectiveUserID
+  when (userID /= 0) $ die "Please run me as root"
 
 mountBackupVolume :: Turtle.FilePath -> Turtle.FilePath -> IO ()
 mountBackupVolume volume mountPoint = do
-    printf ("Mounting backup volume "%fp%" to "%fp%"\n") volume mountPoint
-    mktree mountPoint
-    execute "mount" [format fp volume, format fp mountPoint]
-          $ format ("Failed to mount "%fp%" to "%fp) volume mountPoint
+  printf ("Mounting backup volume " % fp % " to " % fp % "\n") volume mountPoint
+  mktree mountPoint
+  execute "mount" [format fp volume, format fp mountPoint] $
+    format ("Failed to mount " % fp % " to " % fp) volume mountPoint
 
 unmountBackupVolume :: Turtle.FilePath -> IO ()
 unmountBackupVolume mountPoint = do
-    printf ("Unmounting backup volume from "%fp%"\n") mountPoint
-    execute "umount" [format fp mountPoint]
-          $ format ("Failed to unmount "%fp) mountPoint
-    rmdir mountPoint
+  printf ("Unmounting backup volume from " % fp % "\n") mountPoint
+  execute "umount" [format fp mountPoint] $
+    format ("Failed to unmount " % fp) mountPoint
+  rmdir mountPoint
 
 mountLvmSnapshot :: Turtle.FilePath -> Turtle.FilePath -> IO ()
 mountLvmSnapshot snapshot mountPoint = do
-    printf ("Mounting LVM snapshot "%fp%" to "%fp%"\n") snapshot mountPoint
-    mktree mountPoint
-    execute "mount" ["-o", "ro", format fp snapshot, format fp mountPoint]
-          $ format ("Failed to mount "%fp%" to "%fp) snapshot mountPoint
+  printf ("Mounting LVM snapshot " % fp % " to " % fp % "\n") snapshot mountPoint
+  mktree mountPoint
+  execute "mount" ["-o", "ro", format fp snapshot, format fp mountPoint] $
+    format ("Failed to mount " % fp % " to " % fp) snapshot mountPoint
 
 unmountLvmSnapshot :: Turtle.FilePath -> IO ()
 unmountLvmSnapshot mountPoint = do
-    printf ("Unmounting LVM snapshot from "%fp%"\n") mountPoint
-    execute "umount" [format fp mountPoint]
-          $ format ("Failed to unmount "%fp) mountPoint
-    rmdir mountPoint
+  printf ("Unmounting LVM snapshot from " % fp % "\n") mountPoint
+  execute "umount" [format fp mountPoint] $
+    format ("Failed to unmount " % fp) mountPoint
+  rmdir mountPoint
 
 removeLvmSnapshot :: Turtle.FilePath -> IO ()
 removeLvmSnapshot snapshot = do
-    printf ("Removing LVM snapshot "%fp%"\n") snapshot
-    execute "lvremove" ["-f", format fp snapshot]
-          $ format ("Failed to remove "%fp) snapshot
+  printf ("Removing LVM snapshot " % fp % "\n") snapshot
+  execute "lvremove" ["-f", format fp snapshot] $
+    format ("Failed to remove " % fp) snapshot
 
 backupBootPartition :: Turtle.FilePath -> Turtle.FilePath -> IO ()
 backupBootPartition mountPoint backupDir = do
-    printf ("Backing up boot partition "%fp%" to "%fp%"\n") mountPoint backupDir
-    void $ proc "rm" ["-rf", format fp backupDir] empty
-    mktree backupDir
-    files <- fold (ls mountPoint) list
-    execute "cp" ("-rp" : (format fp <$> files) <> [format fp backupDir])
-          $ format ("Failed to back up "%fp%" to "%fp) mountPoint backupDir
+  printf ("Backing up boot partition " % fp % " to " % fp % "\n") mountPoint backupDir
+  void $ proc "rm" ["-rf", format fp backupDir] empty
+  mktree backupDir
+  files <- fold (ls mountPoint) list
+  execute "cp" ("-rp" : (format fp <$> files) <> [format fp backupDir]) $
+    format ("Failed to back up " % fp % " to " % fp) mountPoint backupDir
 
 backupRootLvmSnapshot :: Turtle.FilePath -> Turtle.FilePath -> IO ()
 backupRootLvmSnapshot snapshot backupFile = do
-    printf ("Backing up LVM snapshot "%fp%" to "%fp%"\n") snapshot backupFile
-    mktree (parent backupFile)
-    execute "fsarchiver" [ "-o"
-                         , "-Z", "1"
-                         , "-j", "4"
-                         , "savefs"
-                         , format fp backupFile
-                         , format fp snapshot
-                         ]
-          $ format ("Failed to back up "%fp%" to "%fp) snapshot backupFile
+  printf ("Backing up LVM snapshot " % fp % " to " % fp % "\n") snapshot backupFile
+  mktree (parent backupFile)
+  execute
+    "fsarchiver"
+    [ "-o",
+      "-Z",
+      "1",
+      "-j",
+      "4",
+      "savefs",
+      format fp backupFile,
+      format fp snapshot
+    ]
+    $ format ("Failed to back up " % fp % " to " % fp) snapshot backupFile
 
 backupHomeLvmSnapshot :: Turtle.FilePath -> Turtle.FilePath -> IO ()
 backupHomeLvmSnapshot mountPoint backupDir = do
-    printf ("Backing up LVM snapshot "%fp%" to "%fp%"\n") mountPoint backupDir
-    mktree backupDir
-    execute "rsync" [ "-aAX"
-                    , "--delete"
-                    , "--info=progress2,stats2"
-                    , "--exclude=/lost+found"
-                    , format fp mountPoint <> "/"
-                    , format fp backupDir
-                    ]
-          $ format ("Failed to back up "%fp%" to "%fp) mountPoint backupDir
+  printf ("Backing up LVM snapshot " % fp % " to " % fp % "\n") mountPoint backupDir
+  mktree backupDir
+  execute
+    "rsync"
+    [ "-aAX",
+      "--delete",
+      "--info=progress2,stats2",
+      "--exclude=/lost+found",
+      format fp mountPoint <> "/",
+      format fp backupDir
+    ]
+    $ format ("Failed to back up " % fp % " to " % fp) mountPoint backupDir
 
 showSuccessMessage :: IO ()
 showSuccessMessage = echo "Yay! Backup has been successfully completed!"
 
 execute :: Text -> [Text] -> Text -> IO ()
 execute command args errorText =
-    void $ proc command args empty .||. die errorText
+  void $ proc command args empty .||. die errorText
 
 main :: IO ()
 main = do
-    let backupVolume     = "/dev/vg_backup/lv_backup"
-        backupMountPoint = "/mnt/backup"
+  let backupVolume = "/dev/vg_backup/lv_backup"
+      backupMountPoint = "/mnt/backup"
 
-        bootMountPoint   = "/boot"
-        bootBackupDir    = backupMountPoint </> "boot"
+      bootMountPoint = "/boot"
+      bootBackupDir = backupMountPoint </> "boot"
 
-        rootSnapshot     = "/dev/vg_root/snap_root"
-        rootBackupFile   = backupMountPoint </> "root.fsa"
+      rootSnapshot = "/dev/vg_root/snap_root"
+      rootBackupFile = backupMountPoint </> "root.fsa"
 
-        homeSnapshot     = "/dev/vg_home/snap_home"
-        homeMountPoint   = "/mnt/snap_home"
-        homeBackupDir    = backupMountPoint </> "home"
+      homeSnapshot = "/dev/vg_home/snap_home"
+      homeMountPoint = "/mnt/snap_home"
+      homeBackupDir = backupMountPoint </> "home"
 
-    checkIfRoot
-    mountBackupVolume backupVolume backupMountPoint
-    mountLvmSnapshot homeSnapshot homeMountPoint
-    backupBootPartition bootMountPoint bootBackupDir
-    backupRootLvmSnapshot rootSnapshot rootBackupFile
-    backupHomeLvmSnapshot homeMountPoint homeBackupDir
-    unmountLvmSnapshot homeMountPoint
-    unmountBackupVolume backupMountPoint
-    removeLvmSnapshot rootSnapshot
-    removeLvmSnapshot homeSnapshot
-    showSuccessMessage
+  checkIfRoot
+  mountBackupVolume backupVolume backupMountPoint
+  mountLvmSnapshot homeSnapshot homeMountPoint
+  backupBootPartition bootMountPoint bootBackupDir
+  backupRootLvmSnapshot rootSnapshot rootBackupFile
+  backupHomeLvmSnapshot homeMountPoint homeBackupDir
+  unmountLvmSnapshot homeMountPoint
+  unmountBackupVolume backupMountPoint
+  removeLvmSnapshot rootSnapshot
+  removeLvmSnapshot homeSnapshot
+  showSuccessMessage
--- Cuesplit.hs
+++ Cuesplit.hs
@@ -1,55 +1,63 @@
+import Control.Foldl (list)
 import Control.Monad (filterM)
 import Data.Foldable (for_)
-
-import Control.Foldl (list)
 import Filesystem.Path (replaceExtension)
 import Turtle
 
 scanDirs :: [Turtle.FilePath] -> IO ()
-scanDirs []   = scanDir "."
+scanDirs [] = scanDir "."
 scanDirs dirs = for_ dirs scanDir
 
 scanDir :: Turtle.FilePath -> IO ()
 scanDir dir = do
-    printf ("Scanning directory "%fp%"\n") dir
-    cueSheets <- fold (find (suffix ".cue") dir) list
-    for_ cueSheets processCueSheet
+  printf ("Scanning directory " % fp % "\n") dir
+  cueSheets <- fold (find (suffix ".cue") dir) list
+  for_ cueSheets processCueSheet
 
 processCueSheet :: Turtle.FilePath -> IO ()
 processCueSheet cueSheet = do
-    printf ("Found cue sheet "%fp%"\n") cueSheet
-    let extensions = ["ape", "flac", "wv"]
-    audioFiles <- filterM testfile $ replaceExtension cueSheet <$> extensions
-    case audioFiles of
-        []            -> echo "Warning: audio file not found, skipping"
-        (audioFile:_) -> splitAudioFile cueSheet audioFile
+  printf ("Found cue sheet " % fp % "\n") cueSheet
+  let extensions = ["ape", "flac", "wv"]
+  audioFiles <- filterM testfile $ replaceExtension cueSheet <$> extensions
+  case audioFiles of
+    [] -> echo "Warning: audio file not found, skipping"
+    (audioFile : _) -> splitAudioFile cueSheet audioFile
 
 splitAudioFile :: Turtle.FilePath -> Turtle.FilePath -> IO ()
 splitAudioFile cueSheet audioFile = do
-    printf ("Found audio file "%fp%"\n") audioFile
-    let splitDir = replaceExtension cueSheet "tmp"
-    mktree splitDir
+  printf ("Found audio file " % fp % "\n") audioFile
+  let splitDir = replaceExtension cueSheet "tmp"
+  mktree splitDir
 
-    void $ proc "shnsplit" [ "-d", format fp splitDir
-                           , "-f", format fp cueSheet
-                           , "-o", "flac flac -V --best -o %f -"
-                           , "-t", "%n - %t"
-                           , format fp audioFile
-                           ] empty
-        .||. die (format ("Error: failed to split audio file "%fp) audioFile)
+  void $
+    proc
+      "shnsplit"
+      [ "-d",
+        format fp splitDir,
+        "-f",
+        format fp cueSheet,
+        "-o",
+        "flac flac -V --best -o %f -",
+        "-t",
+        "%n - %t",
+        format fp audioFile
+      ]
+      empty
+      .||. die (format ("Error: failed to split audio file " % fp) audioFile)
 
-    void $ proc "rm" ["-f", format fp (splitDir </> "00 - pregap.flac")] empty
-    splitFiles <- sort $ ls splitDir
-    void $ proc "cuetag.sh" (format fp <$> (cueSheet : splitFiles)) empty
-        .||. die "Error: failed to tag split files"
+  void $ proc "rm" ["-f", format fp (splitDir </> "00 - pregap.flac")] empty
+  splitFiles <- sort $ ls splitDir
+  void $
+    proc "cuetag.sh" (format fp <$> (cueSheet : splitFiles)) empty
+      .||. die "Error: failed to tag split files"
 
-    for_ splitFiles $ \file ->
-        mv file (parent splitDir </> filename file)
-    rmtree splitDir
-    rm cueSheet
-    rm audioFile
+  for_ splitFiles $ \file ->
+    mv file (parent splitDir </> filename file)
+  rmtree splitDir
+  rm cueSheet
+  rm audioFile
 
-    printf ("Successfully split audio file "%fp%"\n") audioFile
+  printf ("Successfully split audio file " % fp % "\n") audioFile
 
 parser :: Parser [Turtle.FilePath]
 parser = many $ argPath "dir" "A directory to recursively scan for cue sheets"
